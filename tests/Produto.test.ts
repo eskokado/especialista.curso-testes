76df1ce7a0072e7db2ce97deae7c2665693f4f0d
eskokado <eskokado@gmail.com>
2024-11-02 14:14:48 -0300

    test: Deve lançar um erro ao tentar criar produto sem nome

--- tests/Produto.test.ts
+++ tests/Produto.test.ts
@@ -39,41 +39,49 @@ describe("Classe Produto", ()=>{
             expect(p).toMatchObject({nome, estoque, precoOriginal, precoAtual: precoOriginal})
         })
 
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto sem nome")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com nome menor que 3")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto sem preÃ§o")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto sem estoque")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com estoque negativo")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com estoque 0")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com precoOriginal negativo")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com preÃ§o 0")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com desconto negativo")
-        test.todo("Deve lanÃ§ar um erro ao tentar criar produto com desconto 0")
+        test("Deve lançar um erro ao tentar criar produto sem nome", () => {
+            const p = () => new ProdutoBuilder()
+                .comEstoque(estoque)
+                .comPrecoOriginal(precoOriginal)
+                .build()
+
+            expect(p).toThrow("Nome não pode ser vazio")
+        })
+
+        test.todo("Deve lançar um erro ao tentar criar produto com nome menor que 3")
+        test.todo("Deve lançar um erro ao tentar criar produto sem preço")
+        test.todo("Deve lançar um erro ao tentar criar produto sem estoque")
+        test.todo("Deve lançar um erro ao tentar criar produto com estoque negativo")
+        test.todo("Deve lançar um erro ao tentar criar produto com estoque 0")
+        test.todo("Deve lançar um erro ao tentar criar produto com precoOriginal negativo")
+        test.todo("Deve lançar um erro ao tentar criar produto com preço 0")
+        test.todo("Deve lançar um erro ao tentar criar produto com desconto negativo")
+        test.todo("Deve lançar um erro ao tentar criar produto com desconto 0")
     });
 
     describe("Ao mudar o desconto", ()=>{
         test.todo("Deve aplicar o desconto fixo corretamente")
         test.todo("Deve aplicar o desconto percentual corretamente")
-        test.todo("Deve lanÃ§ar um erro ao tentar alterar o valor para negativo")
-        test.todo("Deve lanÃ§ar um erro ao tentar alterar o valor para 0")
+        test.todo("Deve lançar um erro ao tentar alterar o valor para negativo")
+        test.todo("Deve lançar um erro ao tentar alterar o valor para 0")
     });
 
     describe("Ao reduzir o estoque", ()=>{
         test.todo("Deve reduzir o estoque corretamente")
-        test.todo("Deve lanÃ§ar um erro ao tentar retirar mais do que o estoque tem")
+        test.todo("Deve lançar um erro ao tentar retirar mais do que o estoque tem")
         test.todo("Deve permitir a retirada da mesma quantidade de itens presente no estoque")
-        test.todo("Deve lanÃ§ar um erro ao tentar retirar um valor negativo")
+        test.todo("Deve lançar um erro ao tentar retirar um valor negativo")
     });
 
     describe("Ao aumentar o estoque", ()=>{
         test.todo("Deve aumentar o estoque corretamente")
-        test.todo("Deve lanÃ§ar um erro ao tentar adicionar um valor negativo")
+        test.todo("Deve lançar um erro ao tentar adicionar um valor negativo")
     });
 
-    describe("Ao obter a descriÃ§Ã£o", ()=>{
-        test.todo("Deve retornar a descriÃ§Ã£o de um produto com desconto fixo")
-        test.todo("Deve retornar a descriÃ§Ã£o de um produto com desconto percentual")
-        test.todo("Deve retornar a descriÃ§Ã£o de um produto sem desconto")
+    describe("Ao obter a descriçÃ£o", ()=>{
+        test.todo("Deve retornar a descriçÃ£o de um produto com desconto fixo")
+        test.todo("Deve retornar a descriçÃ£o de um produto com desconto percentual")
+        test.todo("Deve retornar a descriçÃ£o de um produto sem desconto")
 
     });
 });
